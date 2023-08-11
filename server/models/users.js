4cea786d012f1e18c22ef30792e638df460593b1
Chirag Jethwa <99129009+Chiragjethwa1@users.noreply.github.com>
2023-08-11 12:55:39 -0600

    Files Upload

---
+++ server/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const  userSchema = new mongoose.Schema({
+    title:{
+        type:String,
+ 
+       
+    },
+   
+    post:{
+        type: String,
+        
+    }
+
+
+});
+
+const noteModel = mongoose.model("users",userSchema);
+
+module.exports = noteModel;
